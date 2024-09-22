c7fb7539b3b6324bb9d7f13bb08269cfb8bd3a60
itsnotm3h <thewhit3pages@gmail.com>
2024-09-22 21:49:00 +0800

    V1-bones

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// alert("hello world!");
+
+// 1. Get the user's location
+// 2. Set the
+
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
